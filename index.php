376869322496b43977f034e64ed94d37f15158f8
KeltonKarboviak <kelton.karboviak@gmail.com>
2015-12-24 21:13:59 -0600

    Created an example image Carousel to test.

--- index.php
+++ index.php
@@ -1,30 +1,80 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<!--[if lt IE 7]>      <html lang="en" class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html lang="en" class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html lang="en" class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>WEDDING!</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="">
-        <meta name="viewport" content="width=device-width">
 
         <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 
-        <link rel="stylesheet" href="css/normalize.css">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <!-- Normalize CSS -->
+<!--        <link rel="stylesheet" href="css/normalize.css">-->
+        <!-- Custom CSS -->
         <link rel="stylesheet" href="css/main.css">
+
+        <title>WEDDING!</title>
     </head>
     <body>
         <!--[if lt IE 7]>
             <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
         <![endif]-->
 
-        <!-- Add your site or application content here -->
-        <p>Hello world! This is HTML5 Boilerplate.</p>
 
+        <div class="carousel">
+
+            <div class="slide active-slide">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <img src="img/10630668_10207256312582158_390677188105219688_o.jpg" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="slide">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <img src="img/12028880_10207256313062170_360605019752388227_o.jpg" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="slide">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <img src="img/12039020_10207256312742162_6916524555164949124_o.jpg" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="slider-nav">
+            <a href="#" id="arrow-prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+            <ul class="slider-dots">
+                <li class="dot active-dot">&bull;</li>
+                <li class="dot">&bull;</li>
+                <li class="dot">&bull;</li>
+            </ul>
+            <a href="#" id="arrow-next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+        </div>
+
+
+        <!-- jQuery Script & Fallback -->
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+        <!-- Custom JavaScript -->
         <script src="js/plugins.js"></script>
         <script src="js/main.js"></script>
     </body>
